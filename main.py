67e3faa8c023b1279f8a97673900f213b360f9af
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-09-10 08:11:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 print("I have information for the following planets:")
 
-print("   1. Venus   2. Mars    3. Jupiter")
-print("   4. Saturn  5. Uranus  6. Neptune")
+print("   1. Mercury   2. Venus    3. Earth    4. Mars")
+print("   5. Jupiter   6. Saturn   7. Uranus   8. Neptune")
  
-weight = 185
-planet = 3
+weight = 170
+planet = 6
 
 if planet == 1:
-  weight = weight * 0.91
+ weight = weight * 0.38
 elif planet == 2:
-  weight = weight * 0.38
+  weight = weight * 0.91
 elif planet == 3:
-  weight = weight * 2.34
+ weight = weight
 elif planet == 4:
-  weight = weight * 1.06
+  weight = weight * 0.38
 elif planet == 5:
-  weight = weight * 0.92
+  weight = weight * 2.34
 elif planet == 6:
+  weight = weight * 1.06
+elif planet == 7:
+  weight = weight * 0.92
+elif planet == 8:
   weight = weight * 1.19
 
 print("your weight:", weight)
