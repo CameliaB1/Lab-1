764cb3f90942470a7d8d37785adbf0c6b8cab2f5
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-09-10 08:00:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-print("I have information for the following planets:\n")
+print("I have information for the following planets:")
 
 print("   1. Venus   2. Mars    3. Jupiter")
-print("   4. Saturn  5. Uranus  6. Neptune\n")
+print("   4. Saturn  5. Uranus  6. Neptune")
  
 weight = 185
 planet = 3
-
-# Write an if statement below:
 
 if planet == 1:
   weight = weight * 0.91
