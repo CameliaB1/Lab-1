2dacf4195e073fe40e48d691aced7a105d811d42
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-10-20 19:15:13 -0400

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,10 @@
+states = ["Alabama", "Hawaii", "Massachusetts", "New Mexico", "South Dakota"]
+
+states_lows = [46.5, 67.4, 27.4, 36.1, 19.5]
+
+states_highs = [78.6, 72.2, 68, 71.4, 69.9]
+
+average_states_temp = [0]
+for temp in average_states_temp:
+    average_states_temp = states_lows + states_highs
+print(average_states_temp)
