53378855a0ed76e9ab6587c2bb1a5199eed01a28
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-10-23 11:57:59 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,14 +1,10 @@
 states = ["Alabama", "Hawaii", "Massachusetts", "New Mexico", "South Dakota"]
-
+ 
 states_lows = [46.5, 67.4, 27.4, 36.1, 19.5]
-
+ 
 states_highs = [78.6, 72.2, 68, 71.4, 69.9]
-
-state_temps = zip(states_lows, states_highs, states)
-list_states_temps = list(state_temps)
-
-for temp in list_states_temps[1][0]:
-    if temp <= 20:
-        print("Here is the lowest temp out of the five states")
-
-print(list_states_temps)
+ 
+states_low = [temp for temp in states_lows if temp < 20]
+states_high = [temp1 for temp1 in states_highs if temp1 > 75]
+ 
+print("Out of the five states, ",  states,  ", the lowest temperature see was ", states_low, " F, while the highest the five states normally sees is ", states_high, "F.")
