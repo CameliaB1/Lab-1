e768251dee00de36447bd27b47a9c48e94cd829e
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-11-17 08:31:04 -0500

    Update weatherwebscraper.py

--- weatherwebscraper.py
+++ weatherwebscraper.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
-url = requests.get("https://weather.com/weather/tenday/l/Youngstown+OH?canonicalCityId=2793e44da479328c35f16aaab4bd1a229f11b2dac0caef70d1e9334fbfd08c3b")
-bsp = BeautifulSoup(url.text, "html.parser")
-print(bsp.title.text)
 
-ddata = bsp.find_all("span", class_='DailyContent--daypartDate--2A3Wi')
-tdata = bsp.find_all("span", class_='DailyContent--temp--3d4dn')
-pdata = bsp.find_all("span", class_='DailyContent--value--37sk2')
-wdata = bsp.find_all("span", class_ ='Wind--windWrapper--3aqXJ DailyContent--value--37sk2')    
+city = input("Please enter the city you would like to search: ")
+state = input("Which state is that city located?: ")
+
+url = requests.get("https://weather.com/weather/tenday/l/{city}+{state}?canonicalCityId=2793e44da479328c35f16aaab4bd1a229f11b2dac0caef70d1e9334fbfd08c3b")
+bsp = BeautifulSoup(url.text, "html.parser")
+
+ddata = bsp.find_all("span", class_ = 'DailyContent--daypartDate--2A3Wi')
+tdata = bsp.find_all("span", class_ = 'DailyContent--temp--3d4dn')
+pdata = bsp.find_all("span", class_ = 'DailyContent--value--37sk2')
+wdata = bsp.find_all("span", class_ = 'Wind--windWrapper--3aqXJ DailyContent--value--37sk2')    
 
 for days in ddata:
     days = ddata[0].text
@@ -23,6 +26,6 @@ for percips in pdata:
 for wind in wdata:
     wind = wdata[0].text
 
-print("On " + days + " the temperature in the moring will be " + mtemp + " and the temperature at night will be " + ntemp + ".")
-print("The chance of percipitation in the morning will be " + mpercips + " and at night there will be a " + npercips + " chance percipitation.")
+print("On " + days + " the temperature in the morning is " + mtemp + " and the temperature at night will be " + ntemp + ".")
+print("The chance of percipitation in the morning is " + mpercips + " and at night there will be a " + npercips + " chance of percipitation.")
 print("Wind will be coming " + wind + ".")
