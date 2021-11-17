4cd4b7742aab20c4669aa65f9f67753b1c7e61ed
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-11-17 07:50:41 -0500

    Add files via upload

---
+++ tesla_prices_scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+url = requests.get("https://www.marketwatch.com/investing/stock/tsla")
+bsp = BeautifulSoup(url.text, "html.parser")
+print(bsp.title.text)
+
+cprice = bsp.find_all(class_ = 'intraday__price')
+tstamp = bsp.find_all(class_ = 'intraday__timestamp')
+crate = bsp.find_all(class_ = 'change--percent--q')
+
+for price in cprice:
+    price = cprice[0].text
+
+for time in tstamp:
+    time = tstamp[0].text
+
+for change in crate:
+    change = crate[0].text
+print("Current telsa stock prices is " + price.strip())
+print("ROC of tesla stock prices is " + change.strip())
+print(time.strip())
