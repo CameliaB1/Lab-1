2cc2f2d06607a0464d16aba6535e0ebecb0772b3
CameliaB1 <89617117+CameliaB1@users.noreply.github.com>
2021-12-08 08:15:35 -0500

    Add files via upload

---
+++ Modules.py
@@ -0,0 +1,23 @@
+import random
+rand_num = random.randint(1, 10)
+
+if rand_num == 1:
+    print("You are doing great!")
+elif rand_num == 2:
+    print("Don't give up!")
+elif rand_num == 3:
+    print("You got this!")
+elif rand_num == 4:
+    print("You are one in a million!")
+elif rand_num == 5:
+    print("This is going to be your day!")
+elif rand_num == 6:
+    print("You are the best!")
+elif rand_num == 7:
+    print("Don't stop now, you are almost there!")
+elif rand_num == 8:
+    print("You the man!")
+elif rand_num == 9:
+    print("Reach for the stars!")
+elif rand_num == 10:
+    print("Why stop there, the world is yours!")
